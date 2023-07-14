c40b5ffe77306ededd0d5d4f0306a847ca6cddc7
Dylan Auty <dylan.auty@gmail.com>
2023-07-14 18:15:59 +0100

    Add framerate to title of test, change stream params for sink

--- test_python_webcam.py
+++ test_python_webcam.py
@@ -1,25 +1,29 @@
 import cv2
 import time
 
-#vid = cv2.VideoCapture(0)
-#vid = cv2.VideoCapture('udpsrc host=155.198.116.176 port=5000 caps = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)JPEG, payload=(int)26" ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
-#vid = cv2.VideoCapture('udpsrc port=5000 caps = "application/x-rtp, media=(string)video, clock-rate=(int)90000, encoding-name=(string)JPEG, payload=(int)26" ! rtpjpegdepay ! jpegdec ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
-#vid = cv2.VideoCapture('udpsrc uri=udp://155.198.116.176:5000 auto-multicast=true ! application/x-rtp, media=video, encoding
-#vid = cv2.VideoCapture('udp://127.0.0.1:5000', cv2.CAP_FFMPEG)
-#vid = cv2.VideoCapture('udp://155.198.116.176:5000', cv2.CAP_FFMPEG)
-vid = cv2.VideoCapture('udpsrc port=5000 ! application/x-rtp,encoding-name=H264,payload=06 ! rtph264depay ! avdec_h264 ! videoconvert ! appsink', cv2.CAP_GSTREAMER)
+# Gstreamer pipeline to read RTP packets from port 5000, that it assumes are h264-encoded.
+print("Setting up stream")
 
-print("Starting")
+vid = cv2.VideoCapture('''
+        udpsrc port=5000 
+        ! application/x-rtp,encoding-name=H264,payload=96 
+        ! rtph264depay 
+        ! avdec_h264
+        ! videoconvert 
+        ! appsink sync=false drop=true
+        ''', cv2.CAP_GSTREAMER)
+
+print("Roll until frame is received")
 frame = None
-while frame is None:
-    ret, frame = vid.read()
 
-print("Got frame")
+print("Got frame. Displaying stream.")
 while True:
+    start_time = time.time()
     ret, _ = vid.read(frame)
+    if frame is None:
+        continue
 
-    #cv2.imwrite("webcam_frame.png", frame)
-    cv2.imshow('frame', frame)
+    cv2.imshow(f'Frame {1 / (start_time - time.time())}', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
