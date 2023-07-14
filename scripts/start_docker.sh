1c5fbe1e6922467c4bae2b9c15c27b3e1441c226
Dylan Auty <dylan.auty@gmail.com>
2023-07-14 12:33:35 +0100

    Removed trailing punctuation

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -3,4 +3,4 @@
 # fbf5bc2 is the hash for the relevant L4T image with CUDA, Pytorch, and the Yolov7 dependencies.
 # Repository: nvcr.io/nvidia/l4t-pytorch
 # Tag: r32.7.1-pth1.10-py3-yolo
-docker run -it --rm --runtime nvidia -e DISPLAY=$DISPLAY --network host -v /home/nvidia/data:/media/data -w /media/data fbf5bc2. 
+docker run -it --rm --runtime nvidia -e DISPLAY=$DISPLAY --network host -v /home/nvidia/data:/media/data -w /media/data fbf5bc2
