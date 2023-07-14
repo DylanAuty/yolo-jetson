4e71bb72cfdb748f5341873b23ba93ac19a47dc8
Dylan Auty <dylan.auty@gmail.com>
2023-07-14 12:29:36 +0100

    Gitignore, docker startup script, checkpoints dir

---
+++ scripts/start_docker.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Convenience script to start the Yolov7 Docker container for use with L4T r32.7.1.
+# fbf5bc2 is the hash for the relevant L4T image with CUDA, Pytorch, and the Yolov7 dependencies.
+# Repository: nvcr.io/nvidia/l4t-pytorch
+# Tag: r32.7.1-pth1.10-py3-yolo
+docker run -it --rm --runtime nvidia -e DISPLAY=$DISPLAY --network host -v /home/nvidia/data:/media/data -w /media/data fbf5bc2. 
