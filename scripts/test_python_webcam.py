2d5bc57b6685e4449ecf26650e968c1015101ba3
Dylan Auty <dylan.auty@gmail.com>
2023-07-18 15:24:27 +0100

    Moving things around

--- scripts/test_python_webcam.py
+++ scripts/test_python_webcam.py
@@ -3,52 +3,7 @@ import cv2
 import time
 import threading
 
-class VideoCaptureThreading:
-    # Implementation adapted from https://github.com/gilbertfrancois/video-capture-async
-    def __init__(self, src=0, width=640, height=480):
-        self.src = src
-        self.cap = cv2.VideoCapture(self.src)
-        #self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-        #self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-        self.grabbed, self.frame = self.cap.read()
-        self.started = False
-        self.read_lock = threading.Lock()
-
-    def set(self, var1, var2):
-        self.cap.set(var1, var2)
-
-    def start(self):
-        if self.started:
-            print('[!] Threaded video capturing has already been started.')
-            return None
-        self.started = True
-        self.thread = threading.Thread(target=self.update, args=())
-        self.thread.start()
-        return self
-
-    def update(self):
-        while self.started:
-            grabbed, frame = self.cap.read()
-            with self.read_lock:
-                self.grabbed = grabbed
-                self.frame = frame
-
-    def read(self):
-        with self.read_lock:
-            frame = self.frame.copy()
-            grabbed = self.grabbed
-        return grabbed, frame
-
-    def stop(self):
-        self.started = False
-        self.thread.join()
-
-    def isOpened(self):
-        return self.cap.isOpened()
-
-    def __exit__(self, exec_type, exc_value, traceback):
-        self.cap.release()
-
+from modules import VideoCaptureThreading
 
 if __name__ == "__main__":
     # Gstreamer pipeline to read RTP packets from port 5000, that it assumes are h264-encoded.
