4afdba299bb7e1dbaae86785a4b91cc0dd6e3ae1
Dylan Auty <dylan.auty@gmail.com>
2023-07-22 17:56:11 +0100

    Moved tx script back to using h264

--- scripts/camera_stream_tx.sh
+++ scripts/camera_stream_tx.sh
@@ -10,7 +10,7 @@ fi
 
 echo Streaming to $ip on port $port
 # For converting MJPEG camera output to H264. Can result in dropped/partial frames on receiving end (looks like washed out grey video)
-# gst-launch-1.0 -v v4l2src device=/dev/video0 ! jpegdec ! video/x-raw,framerate=12/1 ! x264enc tune=zerolatency ! rtph264pay ! udpsink host=$ip port=$port
+gst-launch-1.0 -v v4l2src device=/dev/video0 ! jpegdec ! video/x-raw,frame-rate=30/1 ! x264enc tune=zerolatency ! rtph264pay ! application/x-rtp,encoding-name=H264,payload=96 ! udpsink host=$ip port=$port
 
 # For sending MJPEG camera stream directly.
-gst-launch-1.0 -v v4l2src device=/dev/video0 ! image/jpeg,frame-rate=30/1 ! rtpjpegpay ! udpsink host=$ip port=$port
+#gst-launch-1.0 -v v4l2src device=/dev/video0 ! image/jpeg,frame-rate=30/1 ! rtpjpegpay ! udpsink host=$ip port=$port
