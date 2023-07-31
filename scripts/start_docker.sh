2695db1b90a9f8575d8a5090c4c52eb4eb4d33a5
Dylan Auty <dylan.auty@gmail.com>
2023-07-31 14:08:49 +0100

    Change working dir for docker image

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Convenience script to start the Yolov7 Docker container for use with L4T r32.7.1.
-docker run -it --rm --runtime nvidia -e DISPLAY=$DISPLAY --network host -v /home/nvidia/data:/media/data -w /media/data r32.7.2-pth1.10-yolo 
+docker run -it --rm --runtime nvidia -e DISPLAY=$DISPLAY --network host -v /home/nvidia/data:/media/data/yolo-jetson -w /media/data r32.7.2-pth1.10-yolo 
