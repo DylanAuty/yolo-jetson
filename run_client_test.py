455f1b239e0b7c6b6c7a182e0b68e7f33f3b0635
Dylan Auty <dylan.auty@gmail.com>
2023-08-02 18:53:05 +0100

    Added test client

---
+++ run_client_test.py
@@ -0,0 +1,22 @@
+# run_client_test.py
+# Instantiates a JSON-RPC client, then polls it for results as fast as it can.
+import time
+import jsonrpclib
+import json
+
+def main(args):
+    server = jsonrpclib.ServerProxy(args.address)
+    while True:
+        start_time = time.time()
+        response = server.run_inference()
+        #print(response)
+        print(f'FPS: {1 / (time.time() - start_time):4.2f}')
+    
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--address', '-a', default='http://127.0.0.1:8080', help='Address and port of server.')
+
+    args = parser.parse_args()
+    main(args)
