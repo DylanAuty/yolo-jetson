10e27f5c94e4c6bcba5700a003e1b3ccb2ed47a9
Dylan Auty <dylan.auty@gmail.com>
2023-08-03 15:46:36 +0100

    Added log redirection

--- scripts/start_server_headless.sh
+++ scripts/start_server_headless.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Start the Yolov7 Docker container and the JSON-RPC server in the background. For use with L4T r32.7.1.
-docker run -d --restart unless-stopped --runtime nvidia --network host -v /home/nvidia/data:/media/data -w /media/data/yolo-jetson r32.7.2-pth1.10-yolo python3 run_server_sync.py
+docker run -d --restart unless-stopped --runtime nvidia --network host -v /home/nvidia/data:/media/data -w /media/data/yolo-jetson r32.7.2-pth1.10-yolo bash -c "python3 run_server_sync.py > logs/server_log.txt 2>&1"
