788246cfaf6f8ad53641928002376e0adbb2dd3e
Dylan Auty <dylan.auty@gmail.com>
2023-08-03 15:11:19 +0100

    Remove vis option

--- run_client_test.py
+++ run_client_test.py
@@ -9,23 +9,14 @@ def main(args):
     server = jsonrpclib.ServerProxy(args.address)
     while True:
         start_time = time.time()
-        if args.visualise:
-            json_response, annotated_image = server.run_inference(conf=0.25, return_visualisation=True)
-            cv2.imshow('frame', annotated_image)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-            cv2.setWindowTitle('frame', f'FPS: {1 / (time.time() - start_time):4.2f}')
-            print(json_response)
-        else:
-            json_response = server.run_inference()
-            print(json_response)
-            print(f'FPS: {1 / (time.time() - start_time):4.2f}')
+        json_response = server.run_inference()
+        print(json_response)
+        print(f'FPS: {1 / (time.time() - start_time):4.2f}')
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--address', '-a', default='http://127.0.0.1:8080', help='Address and port of server.')
-    parser.add_argument('--visualise', '-v', action='store_true', help='Whether to have the server return annotated frames.')
 
     args = parser.parse_args()
     main(args)
