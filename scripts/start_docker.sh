d997aea4d2ab6578d0948095a9991dd03ca8f478
Dylan Auty <dylan.auty@gmail.com>
2023-08-16 12:13:29 +0100

    Fixed image name

--- scripts/start_docker.sh
+++ scripts/start_docker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# Convenience script to start the Yolov7 Docker container for use with L4T r32.7.2.
-docker run -it --rm --runtime nvidia -e DISPLAY=$DISPLAY --network host -v /home/$USER/Documents/yolo-jetson:/media/yolo-jetson -w /media/yolo-jetson yolo-jetson:latest
+# Convenience script to start the Yolov7 Docker container.
+ver_maj=$(cat /etc/nv_tegra_release | cut -f2 -d ' ' | sed 's/R/r/g')
+ver_min=$(cat /etc/nv_tegra_release | cut -f5 -d ' ' | sed 's/,//g')
+
+docker run -it --rm --runtime nvidia -e DISPLAY=$DISPLAY --network host -v /home/$USER/Documents/yolo-jetson:/media/yolo-jetson -w /media/yolo-jetson yolo-jetson:$ver_maj.$ver_min
--- scripts/start_server_headless.sh
+++ scripts/start_server_headless.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # Start the Yolov7 Docker container and the JSON-RPC server in the background. For use with L4T r32.7.1.
-docker run -d --restart unless-stopped --runtime nvidia --network host -v /home/$USER/Documents/yolo-jetson:/media/yolo-jetson -w /media/yolo-jetson yolo-jetson:latest bash -c "python3 -m pip install jsonrpclib-pelix && python3 run_server_sync.py > logs/server_log.txt 2>&1"
+ver_maj=$(cat /etc/nv_tegra_release | cut -f2 -d ' ' | sed 's/R/r/g')
+ver_min=$(cat /etc/nv_tegra_release | cut -f5 -d ' ' | sed 's/,//g')
+
+docker run -d --restart unless-stopped --runtime nvidia --network host -v /home/$USER/Documents/yolo-jetson:/media/yolo-jetson -w /media/yolo-jetson yolo-jetson:$ver_maj.$ver_min bash -c "python3 run_server_sync.py > logs/server_log.txt 2>&1"
